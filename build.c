ff33f8ea62afd085d8413729abc77f6ce5470167
sum1code <nathanael.adrean@gmail.com>
2023-12-21 12:55:47 +0800

    fixed dir_ops

--- build.c
+++ build.c
@@ -1,8 +1,7 @@
 #define CPM_SUPPRESS_LINKER_WARNING
 #include "CPM.h"
 
-int main(int argc, char** argv)
-{
-    CPM_REBUILD_SELF(argc, argv);
-    cpm_log(CPM_LOG_INFO, "direxists: %d", dir_ops(DIR_CHECK, "./build"));
+int main(int argc, char **argv) {
+  CPM_REBUILD_SELF(argc, argv);
+  cpm_log(CPM_LOG_INFO, "direxists: %d\n", dir_ops(DIR_DELETE, "./build"));
 }
